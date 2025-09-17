034a4253fa3946870651f86a701f9471c4bb2ea4
Maher-Wali <walimaher00@gmail.com>
2025-09-17 23:11:31 +0100

    another attempt

--- Examples/AEGP/Panelator/Win/AEPathLink.r
+++ Examples/AEGP/Panelator/Win/AEPathLink.r
@@ -1,10 +1,20 @@
-#include "AE_Effect.r"
+#include "AE_General.r"
 
+// AEPathLink macOS PiPL
 resource 'PiPL' (16000)
 {
+    // Plugin name shown in AE menus
     name = "AEPathLink";
+
+    // Category of the plugin
     category = "General Plugin";
+
+    // Unique 4-character plugin ID (from Windows rc dnik)
     id = 'dnik';
+
+    // Entry point function exported from the plugin
     entry_point = "EntryPointFunc";
+
+    // Plugin version
     version = 1;
 };
