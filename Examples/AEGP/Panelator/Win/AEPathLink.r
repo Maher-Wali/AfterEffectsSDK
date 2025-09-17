8b0d65f3c0832019f360d3cae4a5cf8363ce0266
Maher-Wali <walimaher00@gmail.com>
2025-09-17 23:41:19 +0100

    switch statement

--- Examples/AEGP/Panelator/Win/AEPathLink.r
+++ Examples/AEGP/Panelator/Win/AEPathLink.r
@@ -3,11 +3,10 @@
 resource 'PiPL' (16000)
 {
     {
-        Name        { "AEPathLink" },          // Display name
-        category    { "General Plugin" },      // Menu category
-        MfgID       { 'dnik' },               // 4-char manufacturer ID
-        TypeID      { 'xgEA' },               // 4-char plugin type ID (unique per plugin)
-        EntryPoint  { "EntryPointFunc" },     // Function exported by plugin
-        Version     { 1 }                      // Plugin version
+        Kind        { 'AEgp' },
+        Name        { "AEPathLink" },
+        Category    { "General Plugin" },
+        EntryPoint  { "EntryPointFunc" },
+        Version     { 1 }
     }
 };
