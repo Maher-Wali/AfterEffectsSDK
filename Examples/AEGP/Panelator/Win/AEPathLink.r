cf49fae2b0959303227659ac5cf2764d3a732e3a
Maher-Wali <walimaher00@gmail.com>
2025-09-17 23:35:23 +0100

    other syntac for general

--- Examples/AEGP/Panelator/Win/AEPathLink.r
+++ Examples/AEGP/Panelator/Win/AEPathLink.r
@@ -3,10 +3,11 @@
 resource 'PiPL' (16000)
 {
     {
-        Name { "AEPathLink" },
-        category { "General Plugin" },
-        id { 'dnik' },            // unique plugin ID
-        entry_point { "EntryPointFunc" },
-        version { 1 }
+        Name        { "AEPathLink" },          // Display name
+        category    { "General Plugin" },      // Menu category
+        MfgID       { 'dnik' },               // 4-char manufacturer ID
+        TypeID      { 'xgEA' },               // 4-char plugin type ID (unique per plugin)
+        EntryPoint  { "EntryPointFunc" },     // Function exported by plugin
+        Version     { 1 }                      // Plugin version
     }
 };
