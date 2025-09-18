bfb2332b45e6114f08dc0d68eb28dbc50841383f
Maher-Wali <walimaher00@gmail.com>
2025-09-18 02:47:08 +0100

    .r upgrade

--- Examples/AEGP/Panelator/Win/AEPathLink.r
+++ Examples/AEGP/Panelator/Win/AEPathLink.r
@@ -1,14 +1,26 @@
-#include <Types.r>  // Standard macOS types (CarbonCore)
+#include "AEConfig.h"
+
+#ifndef AE_OS_WIN
+	#include "AE_General.r"
+#endif
 
 resource 'PiPL' (16000, "AEPathLink", purgeable) {
-    {
-        Kind { AEGP },
-        Name { "AEPathLink" },
-        Category { "General Plugin" },
-        Version { 0x30000 },  // 3.0.0
-        CodeMacIntel64 { "EntryPointFunc" },
-        CodeMacARM64 { "EntryPointFunc" },
-        AE_PiPL_Version { 2, 0 },
-        AE_Effect_Global_OutFlags { 0x02000000 }  // Panel flag
-    }
+	{
+		Kind { AEGP },
+		Name { "AEPathLink" },
+		Category { "General Plugin" },
+		Version { 0x30000 },  // 3.0.0
+#ifdef AE_OS_WIN
+	#ifdef AE_PROC_INTELx64
+		CodeWin64X86 { "EntryPointFunc" },
+	#endif
+#else
+	#ifdef AE_OS_MAC
+		CodeMacIntel64 { "EntryPointFunc" },
+		CodeMacARM64 { "EntryPointFunc" },
+	#endif
+#endif
+		AE_PiPL_Version { 2, 0 },
+		AE_Effect_Global_OutFlags { 0x02000000 }  // Panel flag
+	}
 };
