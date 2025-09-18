7b943f1cdfa00b5db54df39ee77937acfc37b91c
Maher-Wali <walimaher00@gmail.com>
2025-09-18 01:07:12 +0100

    fixes everywhere

--- Examples/AEGP/Panelator/Win/AEPathLink.r
+++ Examples/AEGP/Panelator/Win/AEPathLink.r
@@ -1,11 +1,20 @@
+#include "AEConfig.h"
 #include "AE_General.r"
 
-resource 'PiPL' (16000)
-{
+resource 'PiPL' (16000, "AEPathLink", purgeable) {
     {
-        Kind        { 'AEgp' },
-        Name        { "AEPathLink" },
-        Category    { "General Plugin" },
-        Version     { 1 }
+        Kind { AEGP },
+        Name { "AEPathLink" },
+        Category { "General Plugin" },
+        Version { 196608 },
+        CodeMacIntel64 { "EntryPointFunc" },
+        CodeMacARM64 { "EntryPointFunc" },
+        Creator 'PLTK',
+        AE_PiPL_Version { 2, 0 },
+        AE_Effect_Spec_Version { 13, 8 },
+        AE_Effect_Version { 1 },
+        AE_Effect_Info_Flags { 0 },
+        AE_Effect_Global_OutFlags { 0 },
+        AE_Effect_Global_OutFlags_2 { 0 }
     }
 };
